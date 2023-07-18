ca7ba1aabd4d4bed1f006eb83832fcf0c6cd5326
arnab125 <arnabtasnimpranto@gmail.com>
2023-07-18 12:53:30 +0600

    no of copies

--- library_app/views.py
+++ library_app/views.py
@@ -88,6 +88,7 @@ def borrow_book(request, isbn):
     borrowed_date = datetime.date.today()
     due_date = borrowed_date + datetime.timedelta(days=7)
     borrowed_book = BorrowedBook.objects.create(user=user, book=book, borrowed_date=borrowed_date, due_date=due_date)
+    book.number_of_copies_available -= 1
     return redirect('view_books')
 
 def return_book(request, isbn):
@@ -95,6 +96,7 @@ def return_book(request, isbn):
     book = Book.objects.get(isbn=isbn)
     borrowed_book = BorrowedBook.objects.filter(user=user, book=book)
     borrowed_book.delete()
+    book.number_of_copies_available += 1
     return redirect('view_borrowed_books')
 
 def view_borrowed_books(request):
